f8f73c7be9a2c7320f16d79f2905f718680d829b
vadim-belenya <belenya.vadik@yandex.ru>
2025-02-03 12:51:47 +0600

    написали метод для изменения состояния тигра

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-print("Hello world!")
-def cat():
-    print(cat)
+import random
+
+
+class Tiger:
+    def __init__(self):
+        self.state = "Выследить добычу"  # Атаковать добычу,Бежать домой.
+        self.x = 0
+        self.y = 0
+        self.successful_attack_prob = 0.5
+
+    def update_state(self):
+        if self.state == "Выследить добычу":
+            print("Тигр выслеживает добычу")
+            print("Тигр движется в случайно направлении")  # Позже здесь будет вызвана функция движения
+            print("Проверяем наличиерядом зайца")  # Позже здесь будет вызвана функция проверки на наличие рядом зайца
+            print("Если заяц рядом,то меняем состоянеие на Атаковать добычу")
+        elif self.state == "Атаковать добычу":
+            a = random.random()
+            if a > self.successful_attack_prob:
+                print("Тигр успешно cхватил зайца")
+                # Здесь будет вызвана функция захвата зайца
+                self.state = "Бежать домой"
+            else:
+                print("Заяц убежал.Атака не состоялась")
+        elif self.state == "Бежать домой":
+            print("Тигр возвращается домой с добычей")
+            self.x = 0
+            self.y = 0
 
